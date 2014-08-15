4b4033971015a9bad6b57d8a24113dd3fcbb01f9
CoJaTutajRobie <piotr2791782@wp.pl>
2014-08-15 15:23:45 +0200

    my_places

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -1,69 +1,105 @@
 require 'rails_helper'
 
-
-#describe "controllers/places_controller.rb" do
 RSpec.describe PlacesController, :type => :controller do
 
   setup do
     @place = places(:one)
   end
 
-  #it "should get index" do
-  #  get :index
-#	expect(response).to be_success
-   # assert_not_nil assigns(:places)
- # end
 describe "GET #index" do 
 it "should get index" do
 get :index
 expect(response).to be_success
 end 
+it "check if renders index" do
+get :index
+expect(response).to render_template :index
+end
+it "check if all places load" do
+place1, place2 = Place.create!, Place.create!
+get :index
+expect(assigns(:places)).to match_array([place1,place2])
+end
 end
 
-
- 
+describe "NEW" do
   it "should get new" do
     get :new
-	expect(response).to be_success
+	expect(assigns(:place)).to be_a_new(Place)
   end
+it "respounds success 200" do
+  get :new
+	expect(response).to be_success
+expect(response.status).to eq(200)
+end
+it "renders template" do
+  get :new
+expect(response).to render_template :new
+end
+end
 
-
+describe "CREATE" do
   it "should create place" do
 change(Place, :count) do
-   # assert_difference('Place.count') do
       post :create, place: { address: @place.address, description: @place.description, latitude: @place.latitude, longitude: @place.longitude }
     end
-
-    #assert_redirected_to place_path(assigns(:place))
   end
+end
 
+describe "SHOW" do
   it "should show place" do
      redirect_to :action => "show", :id => @place
-#id: @place
 expect(response).to be_success
   end
+end
 
+describe "EDIT" do
   it "should get edit" do
 redirect_to :action=>"edit", :controller=>"places", :id=>nil
-    #get :edit, id: @place
 expect(response).to be_success
   end
+end
 
+describe "UPDATE" do
   it "should update place" do
 redirect_to :action=>"update", :controller=>"places", :id=>nil, :place=>{:adress=>nil, :description=>nil, :latitude=>nil, :longitude=>nil}
-#@attr = { :adress => @place, :description => @place , :description => @place , :latitude => @place , :longitude => @place }
-#put :update, :id => @place, :place => @attr
-
-  # put :update, :id => @place, place: { address: @place.address, description: @place.description, latitude: @place.latitude, longitude: @place.longitude }
-  #  assert_redirected_to place_path(assigns(:place))
   end
+end
 
+describe "DELETE" do
   it "should destroy place" do
 change(Place, :count).by(-1) do
-    #assert_difference('Place.count', -1) do
       delete :destroy, id: @place
     end
- #assert_redirected_to places_path
   end
+end
+
+describe "invalid params" do 
+it "adres" do
+allow_any_instance_of(Place).to receive(:save).and_return(false)
+post :create, {:place => {"address" => "123"}}
+expect(assigns(:place)).to be_a_new(Place)
+end
+it "opis" do
+allow_any_instance_of(Place).to receive(:save).and_return(false)
+post :create, {:place => {"description" => "@#%$#^#$@"}}
+expect(assigns(:place)).to be_a_new(Place)
+end
+it "latitude" do
+allow_any_instance_of(Place).to receive(:save).and_return(false)
+post :create, {:place => {"latitude" => "asdasd"}}
+expect(assigns(:place)).to be_a_new(Place)
+end
+it "longitude" do
+allow_any_instance_of(Place).to receive(:save).and_return(false)
+post :create, {:place => {"longitude" => "asasfsdg"}}
+expect(assigns(:place)).to be_a_new(Place)
+end
+it "re-renders new" do
+allow_any_instance_of(Place).to receive(:save).and_return(false)
+post :create, {:place => {:adress=>"123", :description=>"@#%$#^#$@", :latitude=>"asdasd", :longitude=>"asasfsdg"}}
+expect(assigns(:place)).to be_a_new(Place)
+end
+end
 end
 
